37c6af9cf388765c9967c1f6d23cc341fed5fa7b
Christian Cricchi <studychristiancricchi@gmail.com>
2024-07-24 11:40:22 +0000

    Added Search functionality

--- app.py
+++ app.py
@@ -30,6 +30,12 @@ def get_flights():
     return render_template("flights.html", flights=flights)
 
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    flights = list(mongo.db.flights.find({"$text": {"$search": query}}))
+    return render_template("flights.html", flights=flights)
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
