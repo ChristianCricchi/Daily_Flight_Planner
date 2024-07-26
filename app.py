488440cfe862a833fe46fe0ea13ebba522fd054e
Christian Cricchi <studychristiancricchi@gmail.com>
2024-07-26 22:15:44 +0000

    fixed error of indentation in app.py

--- app.py
+++ app.py
@@ -66,25 +66,26 @@ def login():
         # Check if username exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
+        
         if existing_user:
             # Ensure hashed password matches user input
             if check_password_hash(
                 existing_user["password"], 
-            request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome {}".format(
-                        request.form.get("username")))
-                    return redirect(url_for(
-                        "profile", username=session["user"])) 
+                request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome {}".format(
+                    request.form.get("username")))
+                return redirect(url_for(
+                    "profile", username=session["user"])) 
             else:
                 # Invalid password match
                 flash("Incorrect Username and/or Password, please try again!")
                 return redirect(url_for("login"))
-
         else:
             # Username doesn't exist
             flash("Incorrect Username and/or Password, please try again!")
             return redirect(url_for("login"))
+    
     return render_template("login.html")
 
 
@@ -268,3 +269,4 @@ if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+            
