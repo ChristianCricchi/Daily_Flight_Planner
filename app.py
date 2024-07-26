4c571faf7a15eed163d29a648bbbc5d5dafff133
Christian Cricchi <studychristiancricchi@gmail.com>
2024-07-26 22:38:41 +0000

    removed trailing whitespace in app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def register():
 
         register = {
             "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password")) 
+                "password": generate_password_hash(request.form.get("password")) 
         }
         mongo.db.users.insert_one(register)  
         # Store the authenticated user's data in a session cookie
